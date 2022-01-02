a0e5370fa4ec3453e91863239c290ce4ac64b225
Ruei-Yuan Lu <RueiYuan.Lu@gmail.com>
2022-01-02 14:52:46 +0800

    Fix lsdep.sh on multiarch system

--- lsdep.sh
+++ lsdep.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-for x in `readelf -d "$1" |grep NEEDED|awk -F'[\[\]]' '{print $2}'` ; do dpkg -S $x|awk -F: '{print $1}' ;done|sort|uniq
+for x in `readelf -d "$1" |grep NEEDED|awk -F'[\[\]]' '{print $2}'` ; do dpkg -S $x|grep amd64|awk -F: '{print $1}' ;done|sort|uniq
 
